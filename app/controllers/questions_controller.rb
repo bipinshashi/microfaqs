f344a9bbd932fa52340b0115480c06529ba28177
bipen sasi <bipinshashi@gmail.com>
2014-05-23 15:50:23 +0400

    added expanded page

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,7 @@
+class QuestionsController < ApplicationController
+
+	def show
+
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Microfaqs::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  resources :questions
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
