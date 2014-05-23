e1850ee14140694ab93f32a23e61284c6148cf76
bipen sasi <bipinshashi@gmail.com>
2014-05-23 12:29:58 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Microfaqs::Application.config.secret_key_base = '18b40e1cbb147eb1fd024911b5593ee37bd1b7ba13bac165df45f1350f09bd4ab603077869ddd668a9b51387eb287f1cab6e688cf47b4d40d70dd82ac04e1351'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Microfaqs::Application.config.session_store :cookie_store, key: '_microfaqs_session'
